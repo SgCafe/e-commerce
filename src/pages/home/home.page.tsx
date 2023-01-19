31e7e67c1fb82589dc964fcabdffaadff0e4091e
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-01-19 18:50:10 -0300

    fix: error alocation files

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 // Components
-import Header from '../../components/header.component'
+import Header from '../../components/header/header.component'
 
 const HomePage = () => {
     return (
